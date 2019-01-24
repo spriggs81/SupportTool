954f6950e850fe3ac79c8745de5e6e1baa1d54c3
John <spriggsjohnw@gmail.com>
2019-01-24 10:19:52 -0800

    updated connection method since random error in node.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var DATABASEURL = process.env.DATABASEURL || "mongodb://localhost/support_tools_
     IP          = process.env.IP || "127.0.0.1";
 
 //setup MongoDB
-mongoose.connect(DATABASEURL, (err) => {
+mongoose.connect(process.env.DATABASEURL, (err) => {
     if(!err){
         console.log("MongoDB Connection Successful!");
     } else {
@@ -86,6 +86,6 @@ app.use("/clients/:id/servers", serverRoutes);
 app.use("/knowledge", knowledgeRoutes);
 
 
-app.listen(PORT, IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Support Tool Is Online!");
 });
